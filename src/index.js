7a991b6336eacea73177e2b5cb93de260287dc96
Dmitrij Feller <ing.d.feller@gmail.com>
2018-08-01 11:50:17 +0200

    start from scratch and add some components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar.js';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import keys from '../config/keys';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY= keys.apiKey;
+const App = () => {
+	return (
+		<div>
+			<SearchBar/>
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
