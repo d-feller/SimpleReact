23736024e845dedc1ae67ebe87c6022b859d2b90
Dmitrij Feller <ing.d.feller@gmail.com>
2018-08-02 09:53:51 +0200

    add logic to components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,12 +9,17 @@ class SearchBar extends Component {
 		};
 	}
 	render(){
-		return <input onChange={this.onInputChange}/>;
+		return (
+			<div className="search-bar">
+				<input
+					onChange = { event => this.onInputChange(event.target.value) }/>
+			</div>
+		);
 	}
 
-	onInputChange(event){
-		console.log(event.target.value);
+	onInputChange(term){
+		this.setState({term});
+		this.props.onSearchTermChange(term);
 	}
-
 }
 export default SearchBar;
